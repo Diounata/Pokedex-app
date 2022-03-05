1709847bfc90cd45c6b1ff6da7a5615d4113b800
Diounata <jonathamcordeiro42@gmail.com>
2022-03-05 20:41:10 -0300

    feat: pokemon page MainCard

---
+++ src/components/PokemonPage/MainCard/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+import { PokemonType } from '../PokemonType';
+
+export function MainCard() {
+  return (
+    <Container>
+      <PokemonType />
+    </Container>
+  );
+}
---
+++ src/components/PokemonPage/MainCard/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+const Container = styled.ScrollView`
+  flex: 1;
+
+  margin: 12px;
+  margin-top: 0;
+
+  background: #fff;
+  border-radius: 8px;
+`;
+
+export { Container };
