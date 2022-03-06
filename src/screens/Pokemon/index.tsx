f111a2d7e01b13b6b9ce7d43874f6d808219df5d
Diounata <jonathamcordeiro42@gmail.com>
2022-03-06 11:24:49 -0300

    feat: Pokemon screen

--- src/screens/Pokemon/index.tsx
+++ src/screens/Pokemon/index.tsx
@@ -1,10 +1,34 @@
-import { Text } from 'react-native';
-import { Container } from './styles';
+import { Container, HeaderContainer, PokemonImgContainer } from './styles';
+import { Image } from 'react-native';
+import { SvgUri } from 'react-native-svg';
+
+import { Header } from '../../components/PokemonPage/Header';
+import { MainCard } from '../../components/PokemonPage/MainCard';
+
+import PokeballPNG from '../../assets/pokeball-background.png';
+import ArrowLeftSVG from '../../assets/arrow-right.svg';
 
 export function Pokemon() {
   return (
-    <Container>
-      <Text>Pokemon page</Text>
+    <Container showsVerticalScrollIndicator={false}>
+      <HeaderContainer>
+        <Image source={PokeballPNG} style={{ position: 'absolute', right: 0 }} />
+        <Header />
+
+        <PokemonImgContainer>
+          <ArrowLeftSVG />
+
+          <SvgUri
+            uri="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/7.svg"
+            width={150}
+            height={150}
+          />
+
+          <ArrowLeftSVG />
+        </PokemonImgContainer>
+      </HeaderContainer>
+
+      <MainCard />
     </Container>
   );
 }
--- src/screens/Pokemon/styles.ts
+++ src/screens/Pokemon/styles.ts
@@ -1,9 +1,29 @@
 import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
-const Container = styled.View`
+const Container = styled.ScrollView`
   flex: 1;
   padding-top: ${getStatusBarHeight()}px;
+
+  background-color: ${({ theme }) => theme.COLORS.TYPE_WATER};
 `;
 
-export { Container };
+const HeaderContainer = styled.View`
+  justify-content: space-between;
+
+  height: 180px;
+
+  margin: 24px;
+`;
+
+const PokemonImgContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  position: relative;
+  top: 50px;
+  z-index: 2;
+`;
+
+export { Container, HeaderContainer, PokemonImgContainer };
